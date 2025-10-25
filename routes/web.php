f7301039ba3cbfc4f3e45d8d789b600a13e8d460
tuta-ktech-dev <trananhtu@k-tech.net.vn>
2025-10-25 10:58:23 +0700

    Add Filament admin routes

--- routes/web.php
+++ routes/web.php
@@ -5,3 +5,10 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+// Filament routes
+Route::middleware(['auth'])->group(function () {
+    Route::get('/admin', function () {
+        return redirect('/admin/dashboard');
+    });
+});
