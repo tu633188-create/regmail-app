08eb958a5029a16ea5ffaace561f2418a511f817
tuta-ktech-dev <trananhtu@k-tech.net.vn>
2025-10-28 23:28:47 +0700

    fix

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -27,7 +27,8 @@ class AppServiceProvider extends ServiceProvider
                 $name,
                 Auth::createUserProvider($config['provider']),
                 $app['session.store'],
-                $app['request']
+                $app['request'],
+                $app['cookie']
             );
         });
     }
